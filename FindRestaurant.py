cae9b049ec34f0d10704dcd6c40df81689b73b44
GregShen <gregshen0925@gmail.com>
2022-10-05 14:35:32 +0800

    add up to 80 choices

--- FindRestaurant.py
+++ FindRestaurant.py
@@ -18,33 +18,47 @@ places = []
 
 
 def findRestaurant():
-    a=random.randint(0,59)
+    randNum=random.randint(0,59)
 
-    if a<20:
-        places_results = gmaps.places_nearby(location='22.9988944,120.2147295',language='zh-TW', radius = 2500, open_now =True , type = 'restaurant')
+    if randNum<20:
+        places_results = gmaps.places_nearby(location='22.9988944,120.2147295', radius = 2500, open_now =True , type = 'restaurant')
         for place in places_results['results']:
             places.append(place['place_id'])
-        chosen_place = places[a]
-    elif 19<a<40:
-        places_results = gmaps.places_nearby(location='22.9988944,120.2147295',language='zh-TW', radius = 2500, open_now =True , type = 'restaurant')
+        chosen_place_hash = places[randNum]
+    if 19<randNum<40:
+        places_results = gmaps.places_nearby(location='22.9988944,120.2147295', radius = 2500, open_now =True , type = 'restaurant')
         time.sleep(2)
         places_results = gmaps.places_nearby(page_token = places_results['next_page_token'])
         for place in places_results['results']:
             places.append(place['place_id'])
-        chosen_place = places[a-20]
+        randNum = randNum-20
+        chosen_place_hash = places[randNum]
+    if 39<randNum<60:
+        places_results = gmaps.places_nearby(location='22.9988944,120.2147295', radius = 2500, open_now =True , type = 'restaurant')
+        time.sleep(2)
+        places_results = gmaps.places_nearby(page_token = places_results['next_page_token'])
+        time.sleep(2)
+        places_results = gmaps.places_nearby(page_token = places_results['next_page_token'])
+        for place in places_results['results']:
+            places.append(place['place_id'])
+        randNum = randNum-40
+        chosen_place_hash = places[randNum]
     else:
-        places_results = gmaps.places_nearby(location='22.9988944,120.2147295',language='zh-TW', radius = 2500, open_now =True , type = 'restaurant')
+        places_results = gmaps.places_nearby(location='22.9988944,120.2147295', radius = 2500, open_now =True , type = 'restaurant')
+        time.sleep(2)
+        places_results = gmaps.places_nearby(page_token = places_results['next_page_token'])
         time.sleep(2)
         places_results = gmaps.places_nearby(page_token = places_results['next_page_token'])
         time.sleep(2)
         places_results = gmaps.places_nearby(page_token = places_results['next_page_token'])
         for place in places_results['results']:
             places.append(place['place_id'])
-        chosen_place = places[a-40]
+        randNum = randNum-60
+        chosen_place_hash = places[randNum]
 
     # print(chosen_place)
 
-    place_details = gmaps.place(place_id= chosen_place , fields= my_fields,language='zh-TW')
+    place_details = gmaps.place(place_id= chosen_place_hash , fields= my_fields,language='zh-TW')
 
     chosen_detail = place_details['result']
 
@@ -52,7 +66,7 @@ def findRestaurant():
 
     chosen_rating = chosen_detail['rating'] if 'rating' in chosen_detail else 0
 
-    photo_reference=chosen_detail['photos'][0]['photo_reference']
+    # photo_reference=chosen_detail['photos'][0]['photo_reference']
 
     # quote = "昂貴程度：%.1f \n評價：%.1f \n地址：%s" % (chosen_price_level,chosen_rating,chosen_detail['vicinity'])
     # print(a)
